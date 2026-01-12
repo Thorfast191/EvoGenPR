24817c6b414106b09e5b9672558843c88b0f749b
Md. Arafat Islam <98916082+Thorfast191@users.noreply.github.com>
2026-01-12 21:37:47 +0600

    fixed resnet encoder not find issue

--- senpr/senpr_model.py
+++ senpr/senpr_model.py
@@ -1,17 +1,26 @@
+import torch
 import torch.nn as nn
+
 from senpr.encoders import resnet_encoder, swin_encoder
 from senpr.fusion import FeatureGrafting, SPFM
+
+
 class SENPR(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
+
+        # Encoders
         self.resnet = resnet_encoder()
         self.swin = swin_encoder()
 
+        # Fusion
         self.fusion = FeatureGrafting(512, 768, 512)
+
+        # Classifier
         self.classifier = nn.Linear(512, num_classes)
 
     def forward(self, x):
-        f1 = self.resnet(x)
-        f2 = self.swin(x)
-        fused = self.fusion(f1, f2)
+        f1 = self.resnet(x)   # [B, 512]
+        f2 = self.swin(x)     # [B, 768]
+        fused = self.fusion(f1, f2)  # [B, 512]
         return self.classifier(fused)
