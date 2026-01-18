46a6378d26c29028fc33b53d17001fe952f07d74
Md. Arafat Islam <98916082+Thorfast191@users.noreply.github.com>
2026-01-18 14:27:28 +0600

    dataset file path fixing

--- data/dataset.py
+++ data/dataset.py
@@ -3,28 +3,33 @@ from PIL import Image
 from torch.utils.data import Dataset
 
 class ChestXrayDataset(Dataset):
-    def __init__(self, df, labels, root_dir, transform=None):
-        """
-        df: pandas DataFrame with column 'Image Index'
-        labels: numpy array [N, num_classes]
-        root_dir: NIH image root directory
-        """
+    def __init__(self, df, labels, root, transform=None):
         self.df = df.reset_index(drop=True)
         self.labels = labels
-        self.root_dir = root_dir
         self.transform = transform
+
+        # 🔹 Build image index
+        self.image_map = {}
+        for shard in os.listdir(root):
+            shard_path = os.path.join(root, shard, "images")
+            if os.path.isdir(shard_path):
+                for fname in os.listdir(shard_path):
+                    self.image_map[fname] = os.path.join(shard_path, fname)
 
     def __len__(self):
         return len(self.df)
 
     def __getitem__(self, idx):
         img_name = self.df.iloc[idx]["Image Index"]
-        img_path = os.path.join(self.root_dir, img_name)
 
+        if img_name not in self.image_map:
+            raise FileNotFoundError(f"Image not found: {img_name}")
+
+        img_path = self.image_map[img_name]
         image = Image.open(img_path).convert("RGB")
-        label = self.labels[idx]
 
         if self.transform:
             image = self.transform(image)
 
+        label = self.labels[idx]
         return image, label
